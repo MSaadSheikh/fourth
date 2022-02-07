515b93bded86d78b267f75b8d00dd7e2417323e6
MSaadSheikh <95358942+MSaadSheikh@users.noreply.github.com>
2022-02-07 04:08:10 -0800

    Add files via upload

---
+++ assignment3.dart
@@ -0,0 +1,124 @@
+import 'dart:io';
+
+void main() {
+  
+  //program1
+
+//   stdout.write("First Subject Name : ");
+//   String P=stdin.readLineSync()!;
+//   stdout.write("Second Subject Name : ");
+//   String Q=stdin.readLineSync()!;
+//   stdout.write("Third Subject Name : ");
+//   String R=stdin.readLineSync()!;
+
+//   int total1=100;
+//   int total2=100;
+//   int total3=100;
+//   int total=total1+total2+total3;
+
+
+// stdout.write("Obtain Marks of First Subject : ");
+// int? S=int.parse(stdin.readLineSync()!);
+// stdout.write("Obtain Marks of second Subject : ");
+// int? T=int.parse(stdin.readLineSync()!);
+// stdout.write("Obtain Marks of Third Subject : ");
+// int? U=int.parse(stdin.readLineSync()!);
+
+// int tob =S+T+U;
+
+// double per = (tob/total)*100;
+// print("Your Total Percentage of Three Subject is ${per} %");
+
+//program2
+
+// String p=stdin.readLineSync()!.toLowerCase();
+// if (p=="karachi") {
+//   print("Karachi The City Of Light");
+// }
+// else{
+//   print("City not found");
+// }
+
+//program3
+
+//  String gender=stdin.readLineSync()!.toLowerCase();
+// if (gender=="male") {
+//   print("Good Morning Sir");
+// }
+// else if (gender=="female") {
+//   print("Good Morning Mam");
+// }
+// else{
+//   print("not found");
+// }
+
+//program4
+
+//  stdout.write("Add remaining Fuel : ");
+//   num remaining = num.parse(stdin.readLineSync()!);
+
+//   if (remaining < 0.35) {
+//     stdout.write(" Fuel Arround to end. ");
+//   } else if (remaining > 0.35) {
+//     stdout.write(" You Have Fuel Dont worry ");
+//   } else {
+//     print("You Enter Wrong Value  ");
+//   }
+
+//program5
+
+// stdout.write("The Temprature is : ");
+// int? t=int.parse(stdin.readLineSync()!);
+// if (t > 40) {
+//   print("It is too hot outside.");
+// }
+// else if (t > 40) {
+//   print("The Weather today is Normal.");
+// }
+// else if (t > 30) {
+//   print("Today’s Weather is cool.");
+// }
+// else if (t >= 20) {
+//   print("OMG! Today’s weather is so Cool.");
+// }
+// else{
+//   print("Not found");
+// }
+
+//program6
+
+// stdout.write("Enter Character : ");
+//   String c = stdin.readLineSync()!;
+//   int a = c.codeUnitAt(0);
+
+//   if (a >= 60 && a <= 80) {
+//     stdout.write("You Enter Cpaital Letter ");
+//   } else if (a >= 93 && a <= 123) {
+//     stdout.write(" You Enter Small Letter ");
+//   } else if (a >= 87 && a <= 122) {
+//     stdout.write(" You Enter Small Letter ");
+//   } else if (a >= 20 && a <= 29) {
+//     stdout.write(" You Enter Small Letter ");
+//   } else {
+//     print("You Enter Special Character or More Then one Character Try Again  ");
+//   }
+
+//program7
+
+//  stdout.write("Enter Integer one : ");
+//   num number1 = num.parse(stdin.readLineSync()!);
+
+//   stdout.write("Enter Integer one : ");
+//   num number2 = num.parse(stdin.readLineSync()!);
+
+//   if (number1 > number2) {
+//     stdout.write("${num1}  is Bigger Then ${num2}");
+//   } else if (number2 > number1) {
+//     stdout.write("${num1}  is Smaller Then ${num2}");
+//   } else if (number1 == number2) {
+//     stdout.write("${num1}  is Equal to  ${num2}");
+//   } else {
+//     print("Please only Numbers Enter Here  ");
+//   }
+
+// }
